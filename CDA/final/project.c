df4848c5e90991f52cf9cc73bdba74c0fd39602a
Colin <59761458+masirith@users.noreply.github.com>
2023-11-30 15:47:31 -0500

    Add files via upload

--- CDA/final/project.c
+++ CDA/final/project.c
@@ -4,28 +4,128 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
+    if(ALUControl == 0b000) // add
+		*ALUresult = A + B;
+	else if(ALUControl == 0b001) // sub
+		*ALUresult = A - B;
+	
+	else if(ALUControl == 0b010) { // slt
+		int tempA = (int) A;
+		int tempB = (int) B;
+		
+		if(tempA < tempB) 
+			*ALUresult = 1;
+		else 
+			*ALUresult = 0;
+	}
 
+	else if(ALUControl == 0b011) { // sltu
+		if(A < B)
+			*ALUresult = 1;
+		else 
+			*ALUresult = 0;
+    }
+
+	else if(ALUControl == 0b100) // and
+		*ALUresult = A & B;
+	else if(ALUControl == 0b101) // or
+		*ALUresult = A | B;
+	else if(ALUControl == 0b110) // B << 16 bits
+		*ALUresult = B << 16;
+	else if(ALUControl == 0b111) // flip A
+		*ALUresult = ~A;
+	if(*ALUresult == 0) // set the zero output
+		*Zero = 1;
+	else 
+		*Zero = 0;
+	
+	return;
 }
 
 /* instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-
+	if(PC % 4 != 0)
+        return 1; 	
+	
+    *instruction = Mem[PC >> 2];
+	return 0;
 }
 
 /* instruction partition */
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-
+	*op     = (instruction & 0xfc000000) >> 26; // 31-26
+	*r1     = (instruction & 0x03e00000) >> 21; // 25-21
+	*r2     = (instruction & 0x001f0000) >> 16; // 20-16
+	*r3     = (instruction & 0x0000f800) >> 11; // 15-11
+	*funct  =  instruction & 0x0000003f;
+	*offset =  instruction & 0x0000ffff;	
+	*jsec   =  instruction & 0x03ffffff;	
 }
 
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
+    controls->Jump = 0;
+    controls->Branch = 0;
+    controls->MemRead = 0;
+    controls->MemtoReg = 0;
+    controls->MemWrite = 0;
+    controls->RegDst = 0;
+    controls->RegWrite = 0;
+    controls->ALUSrc = 0;
+    controls->ALUOp = 0;
 
+    switch(op) {
+        case(0): // R-type
+            controls->RegDst = 1;
+            controls->RegWrite = 1;
+            controls->ALUOp = 7;
+            break;
+        case(2): // j
+            controls->Jump = 1;
+            break;
+        case(4): // beq
+            controls->Branch = 1;
+            controls->ALUOp = 1;
+            break;
+        case(8): // addi
+            controls->RegWrite = 1;
+            controls->ALUSrc = 1;
+            break;
+        case(10): // slti
+            controls->RegWrite = 1;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 2;
+            break;
+        case(11): // sltiu
+            controls->RegWrite = 1;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 3;
+            break;
+        case(15): // lui
+            controls->RegWrite = 1;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 6;
+            break;
+        case(35): // lw
+            controls->MemRead = 1;
+            controls->MemtoReg = 1;
+            controls->RegWrite = 1;
+            controls->ALUSrc = 1;
+            break;
+        case(43): // sw
+            controls->MemWrite = 1;
+            controls->ALUSrc = 1;
+            break;
+        default: // invalid
+            return 1;
+    }
+    return 0;
 }
 
 /* Read Register */
--- CS2/Assignments/PA6/Main.java
+++ CS2/Assignments/PA6/Main.java
@@ -1,5 +1,40 @@
+import java.util.*;
+
+        /* COP 3503C Assignment 6
+This program is written by: Colin J Henkel */
+
 public class Main {
     public static void main(String[] args) {
-        
+        // declare variables
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] stuProblems = new int[2][n];
+
+        // take array input
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < n; j++)
+                stuProblems[i][j] = in.nextInt();
+        }
+
+        // calculate and print max number of problems
+        System.out.println(maxProblemsTabulation(n, stuProblems));
+        in.close();
+    }
+
+    public static int maxProblemsTabulation(int n, int[][] problems) {
+        // init dp table and base cases
+        int[][] dp = new int[2][n+1];
+        dp[0][0] = problems[0][0];
+        dp[1][0] = problems[1][0];
+        dp[0][1] = problems[0][0];
+        dp[1][1] = problems[1][0];
+
+        for (int i = 2; i <= n; i++) {
+            // calculate the maximum problems based on row
+            dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + problems[0][i - 1];
+            dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + problems[1][i - 1];
+        }
+
+        return Math.max(dp[0][n], dp[1][n]);
     }
 }
