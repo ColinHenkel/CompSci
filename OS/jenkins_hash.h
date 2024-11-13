cfbdb88ca2c28669b8a328c06b5a9f013f7c8e46
Colin <59761458+masirith@users.noreply.github.com>
2024-11-13 11:17:42 -0500

    Proper formatting and CV tracking on OS PA2

--- OS/jenkins_hash.h
+++
@@ -1,9 +0,0 @@
-// jenkins_hash.h
-#ifndef JENKINS_HASH_H
-#define JENKINS_HASH_H
-
-#include <stdint.h>
-
-uint32_t jenkins_one_at_a_time_hash(const char *key);
-
-#endif
--- OS/timestamp.h
+++
@@ -1,9 +0,0 @@
-// timestamp.h
-#ifndef TIMESTAMP_H
-#define TIMESTAMP_H
-
-#include <stdint.h>
-
-uint64_t get_timestamp();
-
-#endif
